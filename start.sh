f4bc830db0c196ffbf30d67b395eaea6d6bf249e
achuthvarghese <acvknr@gmail.com>
2021-11-10 16:17:57 +0530

    Check GH CLI is available

--- start.sh
+++ start.sh
@@ -3,6 +3,17 @@
 # Get Repository Name
 REPO_NAME=$1
 
+# Check if GitHub CLI is installed
+gh --version 2>&1 >/dev/null
+GH_IS_AVAILABLE=$?
+
+#  If 0 gh is installed else not (exit)
+if [ ! $GH_IS_AVAILABLE -eq 0 ]; then
+    echo "GitHub CLI is not available"
+    echo "Please view https://github.com/cli/cli#installation for instructions"
+    exit 1
+fi
+
 # Check Repository Name is provided or not
 if [ -z "$1" ]
 then
